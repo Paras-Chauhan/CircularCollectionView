09a6792750f484c5b26d141a946842d4048383b1
Paras Chauahn <nishantchauhan0054@gmail.com>
2018-03-12 19:35:20 +0530

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,50 @@
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+    
+    var numberOfCells = 10
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.collectionViewLayout = CircleLayout()
+        
+        // just for giggles and grins, let's show the insertion of a cell
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.collectionView?.performBatchUpdates({
+                self.numberOfCells += 1
+                self.collectionView?.insertItems(at: [IndexPath(item: 0, section: 0)])
+            })
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.collectionView?.performBatchUpdates({
+                self.numberOfCells -= 1
+                self.collectionView?.deleteItems(at: [IndexPath(item: 0, section: 0)])
+            })
+        }
+    }
+
+    // update collection view if size changes (e.g. rotate device)
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animateAlongsideTransition(in: view, animation: { _ in
+            self.collectionView?.performBatchUpdates(nil)
+        })
+   }
+}
+
+// MARK: UICollectionViewDataSource
+
+extension ViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfCells
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CircleCell", for: indexPath)
+        return cell
+    }
+}
